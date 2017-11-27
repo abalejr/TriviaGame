c55750d647080901d56c499bdee8a9de50c923ed
A.J. Bale <abalejr@gmail.com>
2017-11-27 16:42:37 -0600

    Declare global variables and hide non-starting elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,26 @@
 $(document).ready(function(){
+	var button = $("button");
+
+	var timeAside = $("#time");
+	var $time = timeAside.find("b");
+
+	var questionSection = $("#question");
+	var $question = questionSection.children("h2");
+	var $answer = questionSection.find("li");
+	var $a = $("#a");
+	var $b = $("#b");
+	var $c = $("#c");
+	var $d = $("#d");
+
+	var resultSection = $("#result");
+	var $result = resultSection.children("h3");
+	var $correction = resultSection.find("b");
+	var $resultImg = resultSection.find("img");
+
+	var finishSection = $("#finish");
+	var $correct = $("#correct").children("b");
+	var $incorrect = $("#incorrect").children("b");
+	var $unanswered = $("#unanswered").children("b");
+
+	timeAside.hide();questionSection.hide();resultSection.hide();finishSection.hide();
 });
