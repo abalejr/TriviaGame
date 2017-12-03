39727344a8c7e6de1b9d545cb8ae65bac5108c1a
A.J. Bale <abalejr@gmail.com>
2017-12-03 17:13:20 -0600

    Start button hides button, shows time and question

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
-	var button = $("button");
+    var button = $("button");
 
-	var timeAside = $("#time");
-	var $time = timeAside.find("b");
+    var timeAside = $("#time");
+    var $time = timeAside.find("b");
 
-	var questionSection = $("#question");
-	var $question = questionSection.children("h2");
-	var $answer = questionSection.find("li");
-	var $a = $("#a");
-	var $b = $("#b");
-	var $c = $("#c");
-	var $d = $("#d");
+    var questionSection = $("#question");
+    var $question = questionSection.children("h2");
+    var $answers = questionSection.find("li");
+    var $a = $("#a");
+    var $b = $("#b");
+    var $c = $("#c");
+    var $d = $("#d");
 
-	var resultSection = $("#result");
-	var $result = resultSection.children("h3");
-	var $correction = resultSection.find("b");
-	var $resultImg = resultSection.find("img");
+    var resultSection = $("#result");
+    var $result = resultSection.children("h3");
+    var $correction = resultSection.find("b");
+    var $resultImg = resultSection.find("img");
 
-	var finishSection = $("#finish");
-	var $correct = $("#correct").children("b");
-	var $incorrect = $("#incorrect").children("b");
-	var $unanswered = $("#unanswered").children("b");
+    var finishSection = $("#finish");
+    var $correct = $("#correct").children("b");
+    var $incorrect = $("#incorrect").children("b");
+    var $unanswered = $("#unanswered").children("b");
 
-	timeAside.hide();questionSection.hide();resultSection.hide();finishSection.hide();
+    function Start() {
+        button.hide();
+        timeAside.show();
+        questionSection.show();
+    }
+    timeAside.hide();questionSection.hide();resultSection.hide();finishSection.hide();
+    
+    button.click(Start);
 });
